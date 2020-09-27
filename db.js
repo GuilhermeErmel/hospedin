36e106f92f978aa025f417854993dcb36a7c5be1
Guilherme Ermel <gui_ermel@hotmail.com>
2020-09-27 14:27:14 -0300

    path para o db

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 
+const config = require("./config.json");
 const mongoClient = require("mongodb").MongoClient;
 
-mongoClient.connect("mongodb://localhost", { useUnifiedTopology: true })
+mongoClient.connect(config.dbConStr, { useUnifiedTopology: true })
             .then(conn => global.conn = conn.db("hospedin"))
             .catch(err => console.log(err))
 
